1cd7688f6fa67aca1dbcc109f8faede317da4b7c
MrCesco <109219368+MrCesco@users.noreply.github.com>
2024-05-02 14:07:02 -0300

    separação de "components"

--- kappy-react/src/main.tsx
+++ kappy-react/src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Login from "./components/Login.tsx";
+import Navbar from "./components/Navbar.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Navbar />
+    <Login />
+  </React.StrictMode>
+);
