d2fc0a6549c355547b76946a7c8b708f5e6a7025
= <augustodemeloh@protonmail.com>
2024-06-14 08:57:27 -0300

    chore: remove packages and change main files
    main files changed - related to design of structure or configuration.

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,35 +1,14 @@
 import "./database";
 
 import express from "express";
-import path from "path";
 
-import cors from "cors";
-// import helmet from 'helmet'
 import routes from "./routes/index";
-import errorHandler from "./middlewares/errorHandler";
-import rateLimiter from "./middlewares/rateLimiter";
-
-const whitelist = [];
-
-const corsOptions = {
-  origin(origin, callback) {
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-};
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors(corsOptions));
-// app.use(helmet());
 
-app.use(rateLimiter);
 app.use(routes);
-app.use(errorHandler);
 
 export default app;
