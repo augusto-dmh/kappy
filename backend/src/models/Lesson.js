ca0fed866844df00be3988e31d85373b30eb41e1
= <augustodemeloh@protonmail.com>
2024-06-19 13:09:15 -0300

    feat: lesson defaultScope
    the default scope for lessons include the exercises they're associated and remove the timestamp fields from the returned values.

--- backend/src/models/Lesson.js
+++ backend/src/models/Lesson.js
@@ -1,4 +1,5 @@
-import { Model, DataTypes } from 'sequelize';
+import { Model, DataTypes } from "sequelize";
+import Exercise from "./Exercise";
 
 export default class Lesson extends Model {
   static init(sequelize) {
@@ -9,32 +10,37 @@ export default class Lesson extends Model {
           allowNull: false,
         },
         previousLesson: {
-            type: DataTypes.INTEGER,
-            allowNull: true,
-            references: {
-              model: 'lesson',
-              key: 'id',
-          }
-        }
+          type: DataTypes.INTEGER,
+          allowNull: true,
+          references: {
+            model: "lesson",
+            key: "id",
+          },
+        },
       },
       {
         sequelize,
         modelName: "lesson",
       }
     );
+
+    this.addScope("defaultScope", {
+      include: [{ model: Exercise, as: "exercises" }],
+      attributes: { exclude: ["createdAt", "updatedAt"] },
+    });
   }
   static associate(models) {
     this.hasMany(models.user, {
-      foreignKey: 'checkpoint',
-      as: 'users',
+      foreignKey: "checkpoint",
+      as: "users",
     });
     this.hasMany(models.exercise, {
-      foreignKey: 'lessonId',
-      as: 'exercises',
+      foreignKey: "lessonId",
+      as: "exercises",
     });
     this.belongsTo(models.lesson, {
-      foreignKey: 'previousLesson',
-      as: 'previous',
+      foreignKey: "previousLesson",
+      as: "previous",
     });
   }
 }
