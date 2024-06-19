625eb5e83e6557e065fbf081fca148e84c04a5ef
= <augustodemeloh@protonmail.com>
2024-06-19 01:13:05 -0300

    feat: add application's controllers
    controllers to handle the logic around the application's entities have been added. All of them at the momento following the store,index,show,update,destroy convention.

--- backend/src/controllers/user.js
+++ backend/src/controllers/user.js
@@ -1,120 +1,53 @@
 import User from "../models/User";
-import * as errors from "../validation/errors";
-import ApiError from "../validation/errors/classes/ApiError";
-import stacktrace from "stack-trace";
-import ErrorContext from "../validation/errors/classes/ErrorContext";
-import Avatar from "../models/Avatar";
-import queryString from "query-string";
 
 const store = async (req, res, next) => {
-  const { nickname, birthdate, isAdmin, selectedAvatarId, password, xp } =
-    req.body;
-
   try {
-    const user = await User.create({
-      nickname,
-      birthdate,
-      isAdmin,
-      selectedAvatarId,
-      password,
-      xp,
-    });
+    const user = await User.create(req.body);
 
     res.json(user);
   } catch (err) {
-    const trace = stacktrace.parse(err);
-    const errorContext = new ErrorContext(err, trace);
-
-    next(errorContext);
+    res.json({ error: err });
   }
 };
 
 const index = async (req, res, next) => {
-  const allowedFilters = ["photos", "avatars"];
-  req.query = req.originalUrl.split("?")[1];
-  const { filter } = queryString.parse(req.query, {
-    arrayFormat: "bracket-separator",
-    arrayFormatSeparator: ",",
-  });
-
-  const scopes = filter ? getValidFilters(filter, allowedFilters) : [];
-
   try {
-    const users = await User.scope(
-      "defaultScope",
-      "noPassword",
-      scopes
-    ).findAll();
+    const users = await User.findAll();
 
     res.json(users);
   } catch (err) {
-    const trace = stacktrace.parse(err);
-    const errorContext = new ErrorContext(err, trace);
-
-    next(errorContext);
+    res.json({ error: err });
   }
 };
 
 const show = async (req, res, next) => {
-  const { user } = req;
+  const user = User.findByPk(req.params.id);
 
   res.json({ ...user.dataValues });
 };
 
 const update = async (req, res, next) => {
-  const { user } = req;
-  const { nickname, birthdate, isAdmin, selectedAvatarId, password, xp } =
-    req.body;
+  const user = User.findByPk(req.params.id);
 
   try {
-    const updatedUser = await user.update(
-      nickname,
-      birthdate,
-      isAdmin,
-      selectedAvatarId,
-      password,
-      xp
-    );
+    const updatedUser = await user.update(req.body);
 
     res.json(updatedUser);
   } catch (err) {
-    const trace = stacktrace.parse(err);
-    const errorContext = new ErrorContext(err, trace);
-
-    next(errorContext);
+    res.json({ error: err });
   }
 };
 
 const destroy = async (req, res, next) => {
-  const { user } = req;
+  const user = User.findByPk(req.params.id);
 
   try {
     await user.destroy();
 
     res.json(user);
   } catch (err) {
-    const trace = stacktrace.parse(err);
-    const errorContext = new ErrorContext(err, trace);
-
-    next(errorContext);
+    res.json({ error: err });
   }
 };
 
-function getValidFilters(filter, allowedFilters) {
-  return filter.filter((f) => allowedFilters.includes(f));
-}
-
 export default { store, index, show, update, destroy };
-
-// function getValidFilters(filter, allowedFilters) {
-//   if (!(filter instanceof Array)) {
-//     if (allowedFilters.includes(filter)) {
-//       return [filter];
-//     }
-//     return [];
-//   }
-
-//   return filter.filter((f) => allowedFilters.includes(f));
-// }
-
-// export default { store, index, show, update, destroy };
