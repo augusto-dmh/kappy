ec5c9083357dc905503955c3052efdaae6785d12
augusto-dmh <130018859+augusto-dmh@users.noreply.github.com>
2026-06-02 03:26:06 -0300

    chore(github-app): scaffold github-app module

---
+++ app-modules/github-app/src/Providers/GitHubAppServiceProvider.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Modules\GitHubApp\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class GitHubAppServiceProvider extends ServiceProvider
+{
+    public function register(): void {}
+
+    public function boot(): void {}
+}
