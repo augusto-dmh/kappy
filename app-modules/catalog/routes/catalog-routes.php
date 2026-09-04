83ee52873563bd79db7ca132c8be55c863d2de32
augusto-dmh <augustodemelohenriques@gmail.com>
2026-09-04 18:10:59 -0300

    chore(catalog): remove the demo product module

--- app-modules/catalog/routes/catalog-routes.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\Catalog\Http\Controllers\ProductController;
-
-Route::middleware(['web'])->group(function (): void {
-    Route::get('/catalog', [ProductController::class, 'index'])->name('catalog.index');
-});
